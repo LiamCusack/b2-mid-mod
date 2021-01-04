d147bfdc1728d48042a8028e42d12c984ea3b413
Liam Cusack <LCusack8108@gmail.com>
2021-01-04 09:39:30 -0700

    Add movies relationship to studios, add studio model test

---
+++ spec/models/studio_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Studio, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+  end
+
+  describe 'relationships' do
+    it {should have_many :movies}
+  end
+
+  describe 'methods' do
+  end
+end
